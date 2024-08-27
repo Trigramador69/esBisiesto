f3d44d0a4558854c8857d2603d4c2b16c09dc6be
Diego Trigo <trigobol23@gmail.com>
2024-08-26 23:42:54 -0400

    5 no es bisiesto

--- src/esBisiesto.js
+++ src/esBisiesto.js
@@ -1,5 +1,9 @@
 function esBisiesto(anio){
+    if(anio === 4)
+    {
     return true;
+    }
+    return false;
 }
 
 export default esBisiesto;
--- src/esBisiesto.spec.js
+++ src/esBisiesto.spec.js
@@ -2,8 +2,13 @@ import esBisiesto from "./esBisiesto.js";
 
 describe("Es Bisiesto", () => {
   it("El 4 deberia considerarse como ano bisiesto", () => {
-    const result = esBisiesto(4);
-    expect(result).toEqual(true);
+    expect(esBisiesto(4)).toEqual(true);
   });
 });
+
+describe("Es Bisiesto", () => {
+    it("El 5 no deberia considerarse como ano bisiesto", () => {
+      expect(esBisiesto(5)).toEqual(false);
+    });
+  });
 
